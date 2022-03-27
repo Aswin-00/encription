d7f15d68b13aaa737255fe3cfb26dbd6cb55b1a5
Bootpq <87950210+Bootpq@users.noreply.github.com>
2022-03-27 12:38:57 +0530

    encription
    simple project to understand encription

--- encription.py
+++ encription.py
@@ -1,13 +1,13 @@
-import random
-f=open('image.txt','r') #put the path on the file  here 
-w=open('write.txt','a') #put the path were u want to save the file
-ent=["%","^","&","$","#","@","!"]
+import random  #only works for txt
+f=open('.txt','r') #put the path of the file  here 
+w=open('.txt','a') #put the path were u want to save the file
+ent=["%","^","&","$","#","@","!"] 
 for u in f:
     v=list(u)
     w.write("\n")
     for i in range(0,len(u)):
         if u[i]==" ":
-            w.write('_')
+            w.write('tan60')  #space are replace by tan60
         else:
             w.write(str(u[i])+ent[random.randint(0,(len(ent)-1))])
 
